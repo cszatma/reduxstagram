ab3250e6928720d12869dea41c4f981ed3eb132c
cszatma <christopherszatmary@gmail.com>
2018-01-27 16:37:51 -0500

    Single now displays the Photo and Comments components

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
+import Photo from './Photo';
+import Comments from './Comments';
+
 export default class Single extends Component {
     render() {
+        const index = this.props.posts.findIndex(post => post.code === this.props.params.postId);
+        const post = this.props.posts[index];
+
         return (
             <div className="single-photo">
-                I'm the single
+                <Photo index={index} post={post} {...this.props} />
+                <Comments />
             </div>
         );
     }
