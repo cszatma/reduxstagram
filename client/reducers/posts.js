207755d814e788923c0402b2eff395bc5da1e58b
cszatma <christopherszatmary@gmail.com>
2018-01-27 16:22:46 -0500

    Completed posts reducer so it now updates the like count state

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,4 +1,17 @@
+import { actionTypes } from '../actions/actionCreators';
+
 export default function posts(state = [], action) {
-    console.log(state, action);
-    return state;
+    if (action.type === actionTypes.incrementLikes) {
+        const index = action.index;
+        return [
+            ...state.slice(0, index),
+            {
+                ...state[index],
+                likes: state[index].likes + 1,
+            },
+            ...state.slice(index + 1),
+        ];
+    } else {
+        return state;
+    }
 }
