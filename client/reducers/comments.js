885e41b305e227ff7692e85c4bc93fd9ab686076
cszatma <christopherszatmary@gmail.com>
2018-01-27 17:18:55 -0500

    Completed comments reducer so that it now performs the necessary action

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,4 +1,29 @@
+import {actionTypes} from '../actions/actionCreators';
+
+function postComments(state = [], action) {
+    switch (action.type) {
+        case actionTypes.addComment:
+            return [...state, {
+                user: action.author,
+                text: action.comment,
+            }];
+        case actionTypes.removeComment:
+            return [
+                ...state.slice(0, action.index),
+                ...state.slice(action.index + 1)
+            ];
+        default:
+            return state;
+    }
+}
+
 export default function comments(state = [], action) {
-    console.log(state, action);
+    if (typeof action.postId !== 'undefined') {
+        return {
+            ...state,
+            [action.postId]: postComments(state[action.postId], action),
+        };
+    }
+
     return state;
 }
