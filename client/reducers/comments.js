3b64a75413bcc11fe4e775e2917333d3dec66f8d
cszatma <christopherszatmary@gmail.com>
2018-01-27 13:39:39 -0500

    Added reducers for posts and comments

---
+++ client/reducers/comments.js
@@ -0,0 +1,4 @@
+export default function comments(action, state = []) {
+    console.log(action, state);
+    return state;
+}
---
+++ client/reducers/posts.js
@@ -0,0 +1,4 @@
+export default function posts(action, state = []) {
+    console.log(action, state);
+    return state;
+}
